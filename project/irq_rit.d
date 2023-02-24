e9b957735d6881dbda45491d8b9f4d0a3080da51
Daniele De Rossi <114685212+therossee@users.noreply.github.com>
2023-02-24 13:07:38 +0100

    3rd part

---
+++ project/irq_rit.d
@@ -0,0 +1,17 @@
+.\irq_rit.o: RIT\IRQ_RIT.c
+.\irq_rit.o: C:\Keil_v5\ARM\PACK\Keil\LPC1700_DFP\2.7.1\Device\Include\lpc17xx.h
+.\irq_rit.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.8.0\CMSIS\Core\Include\core_cm3.h
+.\irq_rit.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\irq_rit.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_version.h
+.\irq_rit.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_compiler.h
+.\irq_rit.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_armcc.h
+.\irq_rit.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.8.0\CMSIS\Core\Include\mpu_armv7.h
+.\irq_rit.o: C:\Keil_v5\ARM\PACK\Keil\LPC1700_DFP\2.7.1\Device\Include\system_LPC17xx.h
+.\irq_rit.o: RIT\RIT.h
+.\irq_rit.o: RIT\../tamagotchi.h
+.\irq_rit.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+.\irq_rit.o: RIT\../GLCD/GLCD.h
+.\irq_rit.o: RIT\../TouchPanel/TouchPanel.h
+.\irq_rit.o: RIT\../music/music.h
+.\irq_rit.o: RIT\../timer/timer.h
+.\irq_rit.o: RIT\../adc/adc.h
---
+++ project/irq_timer.d
@@ -0,0 +1,17 @@
+.\irq_timer.o: timer\IRQ_timer.c
+.\irq_timer.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+.\irq_timer.o: C:\Keil_v5\ARM\PACK\Keil\LPC1700_DFP\2.7.1\Device\Include\lpc17xx.h
+.\irq_timer.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.8.0\CMSIS\Core\Include\core_cm3.h
+.\irq_timer.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\irq_timer.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_version.h
+.\irq_timer.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_compiler.h
+.\irq_timer.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_armcc.h
+.\irq_timer.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.8.0\CMSIS\Core\Include\mpu_armv7.h
+.\irq_timer.o: C:\Keil_v5\ARM\PACK\Keil\LPC1700_DFP\2.7.1\Device\Include\system_LPC17xx.h
+.\irq_timer.o: timer\timer.h
+.\irq_timer.o: timer\../GLCD/GLCD.h
+.\irq_timer.o: timer\../TouchPanel/TouchPanel.h
+.\irq_timer.o: timer\../tamagotchi.h
+.\irq_timer.o: timer\../music/music.h
+.\irq_timer.o: timer\../adc/adc.h
+.\irq_timer.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
